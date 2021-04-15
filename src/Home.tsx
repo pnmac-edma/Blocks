e2e6fb06c79a276baccc906e37cc8431e9e342db
sfowles <sfowles@gmail.com>
2021-04-15 04:31:13 -0700

    Remove unused components

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
 import { color, font } from '@edma/design-tokens';
-import Button from './components/Button/Button';
 
 const useStyles = makeStyles(theme => ({
   version: {
